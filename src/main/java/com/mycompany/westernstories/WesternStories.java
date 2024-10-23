563a026424dfd9887c60a3163bef9d5c01587b61
Marco FORDELONE <72397460+LeThorgrim@users.noreply.github.com>
2024-10-23 11:10:54 +0200

    character creation done

--- src/main/java/com/mycompany/westernstories/WesternStories.java
+++ src/main/java/com/mycompany/westernstories/WesternStories.java
@@ -49,6 +49,7 @@ public class WesternStories {
         
         //CHARACTER CREATION
         //name
+        myStory.blankLn(5);
         String fullName = "null"; //should be changed in the scanner, but throwing me warning if not put
         Scanner scanner = new Scanner(System.in);
         boolean validInput = false;
@@ -63,10 +64,24 @@ public class WesternStories {
                 if (nameParts.length != 2) {
                     throw new InputMismatchException("You must enter exactly two words for name and surname.");
                 }
+                // Check each character in the string
+                boolean onlyLetters = true;
+                for (int i = 0; i < fullName.length(); i++) {
+                    onlyLetters = Character.isLetter(fullName.charAt(i)) || fullName.charAt(i) == ' ';
+                    if (!onlyLetters) {
+                        break;
+                    }
+                }
+                if(!onlyLetters){
+                    throw new InputMismatchException("You must enter only letters.");
+                }
 
-                // Extract name and surname
-                String name = nameParts[0];
-                String surname = nameParts[1];
+                //nameParts = "0";
+                // Extract name and surname; 1st letter uppercase, rest to lower
+                String name = nameParts[0].substring(0, 1).toUpperCase() + nameParts[0].substring(1).toLowerCase();
+                String surname = nameParts[1].substring(0, 1).toUpperCase() + nameParts[1].substring(1).toLowerCase();
+                //recompose the fullName String
+                fullName = name + " " + surname;
 
                 // Output the name and surname
                 System.out.println("Name: " + name);
@@ -107,16 +122,13 @@ public class WesternStories {
         }
         scanner.close();
         Bandit player = new Bandit(fullName, favDrink, "player", "");
-        //END CHARACTER CREATION
         
-        //USE GARBAGE CLEANER (not done)
+        myStory.blankLn(5);
+        System.out.println("Welcome to Western Stories " + player.getName());
+        //END OF CHARACTER CREATION
         
-        //tests
-        myStory.blankLn(10);
-        System.out.println("player settings:");
-        System.out.println("name " + player.getName());
-        System.out.println("favdrink " + player.getBFav());
-        System.out.println("attribut " + player.getAttribut());
-        System.out.println("nickname " + player.getNickname());
+        //FOR GARBAGE CLEANER TO DETECT MORE EASILY
+        fullName = null;
+        favDrink = null;
     }
 }
